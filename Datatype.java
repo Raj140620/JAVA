e443573d125c76651269900ada113fb94ab18caf
Raj Kumar Behera <rkb6372649807@gmail.com>
2024-05-20 16:24:09 +0530

    My First commit

---
+++ Datatype.java
@@ -0,0 +1,34 @@
+class Datatype {
+    public static void main(String[] args) {
+        Byte A;    //  (2**8) Stores Data Between -128 to 127 
+        short B;   //  (2**16) Stores Data Between -32768 to 32767
+        int C;     //  (2**32) 
+        long D;    // (2**64) 
+        char E;    // Characters and Special Characters like A , B,a ,d , $,#
+        double F;  // Stores Double Numbers 2.3534636 ,65.7965656 etc.
+        float G;   // Stores Floating Point Numbers 2.35 , 65.79
+        boolean H; // Stores True and False
+
+        A = 127;
+        B = -200;
+        C = 7576587;
+        D = -763296;
+        E = 66;
+        F = -42.4;
+        G = -42.5f;
+        H = true;
+System.out.println("Raj Kumar");
+System.out.println(4+5);
+System.out.println(A);
+System.out.println(B);
+System.out.println(C);
+System.out.println(D);
+System.out.println(E);
+System.out.println(F);
+System.out.println(G);
+System.out.println(H);
+    }
+
+}
+
+
---
+++ Operators.java
@@ -0,0 +1,8 @@
+// Types Of Operators in JAVA 
+
+
+// Assignment Operator[s In Java
+public  Operators{
+ Arithmatic_Opertors()
+
+}
