18199fd3dfcd02509f5c7bde2d249dc141a303eb
Raj Kumar Behera <rkb6372649807@gmail.com>
2024-05-20 17:16:25 +0530

    Worked On JAVA Arithmetic Operators

--- Operators.java
+++ Operators.java
@@ -3,16 +3,25 @@ public  class Operators{
     public static void main(String[]args){
         Arithmetic_Operators();
     }
-// Assignment Operators In Java
+// Arithmetic Operators In Java
 public static void Arithmetic_Operators(){
     int a = 20;
     int b = 10;
 
-    System.out.println(a+b);
-    System.out.println(a-b);
-    System.out.println(a*b);
-    System.out.println(a/b);
-    System.out.println(a%b);
+    System.out.println("The Addition Of a and b is : " + (a+b));
+    System.out.println("The Subtraction Of a and b is : " + (a-b));
+    System.out.println("The Multiplication Of a and b is : " + (a*b));
+    System.out.println("The Division Of a and b is : " + (a/b));
+    System.out.println("The Modulo Of a and b is : " + (a%b));
 }
+
+// Assignment Operators In Java
+// public static void Assignment_Operators(){
+//     int c = 30;
+//     int d = 20;
+//     System.out.println("The Modulo Of a and b is : " + (c));
+    
+
+// }
 
 }
