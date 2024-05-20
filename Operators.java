76060b5e90d4be162d9d521adca89eef517b11ed
Raj Kumar Behera <rkb6372649807@gmail.com>
2024-05-20 16:49:38 +0530

    Updated

--- Operators.java
+++ Operators.java
@@ -1,8 +1,18 @@
 // Types Of Operators in JAVA 
+public  class Operators{
+    public static void main(String[]args){
+        Arithmetic_Operators();
+    }
+// Assignment Operators In Java
+public static void Arithmetic_Operators(){
+    int a = 20;
+    int b = 10;
 
-
-// Assignment Operator[s In Java
-public  Operators{
- Arithmatic_Opertors()
+    System.out.println(a+b);
+    System.out.println(a-b);
+    System.out.println(a*b);
+    System.out.println(a/b);
+    System.out.println(a%b);
+}
 
 }
