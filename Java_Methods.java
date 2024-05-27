845d98058030cadfaae7842904a262d6bfdbca2c
Raj Kumar Behera <rkb6372649807@gmail.com>
2024-05-27 10:51:35 +0530

    Working With Basic Java Methods

--- Java_Methods.java
+++ Java_Methods.java
@@ -2,12 +2,20 @@ public class Java_Methods{
 
     static String hello(){
         //return(14);           // ------> I Have To Specify The Int Data Type Of Method TO store My Integers .
-        return("Hello World"); // ------> I Have To Specify The String Data Type Of Method TO store My Strings .  
+        return("Hello World"); // ------> I Have To Specify The String Data Type Of Method TO store My Strings .    
      }
+
+     static void Raj(){
+        int Number = 23;
+        String Word = "Raj Kumar";
+        System.out.println(Number);
+        System.out.println(Word);
+    }
     public static void main(String arg[]){
          // Learning Basics Of Java Methods (Also Known As Functions)
           String a = hello();
           System.out.println(a);
+          Raj();
          
     }
 }
