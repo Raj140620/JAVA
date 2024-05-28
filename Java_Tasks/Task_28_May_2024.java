5c719b73753e7521acb817d265455752ed2a5677
Raj Kumar Behera <rkb6372649807@gmail.com>
2024-05-28 16:35:44 +0530

    Working With Java Inputs

---
+++ Java_Tasks/Task_28_May_2024.java
@@ -0,0 +1,66 @@
+class Task_28_May_2024 {
+    public static void main(String[] arg) {
+        // Area_Of_Circle;              // ------------->  Un-Comment This And The Below Function To Run This 
+        //Greet();                     // ------------->  Un-Comment This And The Below Function To Run This 
+        //Area_Of_Rectangle();;         // ------------->  Un-Comment This And The Below Function To Run This 
+    }
+
+    // ---------------> Question -1 <-----------------
+
+
+    // public static void Greet(){
+    //     System.err.println("-----------------   Question N0 - 1    -----------------");
+    //     System.err.println(" ---------  Greeting To The User  ---------"); 
+    //     java.util.Scanner Data = new java.util.Scanner(System.in);
+    //     System.out.println("Please Enter Your Name : ");
+    //     String Name = Data.nextLine();
+    //     System.out.println("Please Enter Your Age : ");
+    //     int Age = Data.nextInt();
+    //     System.out.println("Hello Mr/Mrs " + Name +", Nice To Meet You. I Can't Even Believe That You're Just " + Age + " Years Old.");
+    // }
+
+
+    // ---------------> Question -2 <-----------------
+
+
+    // public static void Area_Of_Rectangle(){
+    //     System.err.println("-----------------   Question N0 - 2    -----------------");
+    //     System.err.println(" ---------  Area Of Rectangle  ---------"); 
+
+    //     java.util.Scanner Length_Breadth = new java.util.Scanner(System.in);
+
+    //     System.out.println("Please Enter The Length Of The Rectangle : ");
+
+    //     int Length = Length_Breadth.nextInt();
+
+    //     System.out.println("Please Enter The Breadth Of The Rectangle : ");
+    //     int Breadth = Length_Breadth.nextInt();
+
+    //     System.out.println("The Area Of The Rectangle Is " + (Length*Breadth));
+
+    // }
+
+    // ---------------> Question -3 <-----------------
+
+
+    // public static void Area_Of_Circle() {
+    //     System.err.println("-----------------   Question N0 - 1    -----------------");
+    //     System.err.println(" ---------  Area Of Circle  ---------");
+    //     Double Area;
+
+    //     java.util.Scanner Number = new java.util.Scanner(System.in);
+      
+    //     System.out.println("Please Enter Any Number");
+      
+    //     Double Radius_Number = Number.nextDouble();
+      
+    //     System.out.println(Radius_Number);
+        
+        
+      
+    //     Area = ((22.0 / 7.0) * (Radius_Number * Radius_Number));
+      
+    //     System.out.println("You Entered  " + Radius_Number + ". So The Area Of The Circle Is  : " + Area);
+    // }
+
+}
