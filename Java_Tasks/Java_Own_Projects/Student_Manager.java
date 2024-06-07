7e2a213eeaee7d87e1edfc96f11ad727522b626a
Raj Kumar Behera <rkb6372649807@gmail.com>
2024-06-07 17:24:28 +0530

    Added More Features

--- Java_Tasks/Java_Own_Projects/Student_Manager.java
+++ Java_Tasks/Java_Own_Projects/Student_Manager.java
@@ -5,14 +5,6 @@ public class Student_Manager {
     }
 
     public static void  Test1(){
-        System.out.println("Hello 1");
-    }
-    public static void Test2(){
-        System.out.println("Hello 2 But From Test3");
-        
-    }
-    public static void Test3(){
-        Test2();
-    }
+
     
 }
